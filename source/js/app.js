bffc68e542ac915eca48f320d1d39fdd4adae04a
Wei-Meng Lee <weimeng@staticfusion.org>
2014-12-16 20:53:23 +0800

    Make site header start out static, then fixed on scroll down

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,14 @@
 $(document).foundation();
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var headerHeight = $('header').height();
+
+    if ($(this).scrollTop() > headerHeight) {
+      $('header').css('position', 'fixed');
+
+    } else if ($(this).scrollTop() <= headerHeight) {
+      $('header').css('position', 'static');
+    }
+  });
+});
