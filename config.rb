4332e41785f972bf4ea44480ad63aa8a1279bc09
Wei-Meng Lee <weimeng@staticfusion.org>
2014-12-24 02:37:42 +0800

    Fix sprite path in CSS

--- config.rb
+++ config.rb
@@ -1,4 +1,4 @@
 images_path = './assets/img'
 sprite_load_path = "./source/sprites"
 generated_images_dir = "./assets/img/sprites"
-http_generated_images_path = "../assets/img/sprites"
+http_generated_images_path = "../../assets/img/sprites"
