13c24a15134445fd1631a696f74a5431857d49a7
Wei-Meng Lee <weimeng@staticfusion.org>
2014-12-23 19:00:03 +0800

    Bootstrap Silex

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+  'twig.path' => __DIR__.'/views',
+));
+$app->register(new SilexMtHaml\MtHamlServiceProvider());
+
+$app->run();
