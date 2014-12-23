7a52d1df77abce0e78dd75b5af09097e3f7f9409
Wei-Meng Lee <weimeng@staticfusion.org>
2014-12-23 21:33:34 +0800

    Initial work on home page

--- web/index.php
+++ web/index.php
@@ -4,9 +4,17 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
+$app['debug'] = true;
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-  'twig.path' => __DIR__.'/views',
+    'twig.path' => __DIR__.'/../views',
 ));
 $app->register(new SilexMtHaml\MtHamlServiceProvider());
+
+// Routes
+
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('homepage.html.haml');
+});
 
 $app->run();
