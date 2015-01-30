d1701cd962a3eed876b3b1d04080a130a676b38d
Sheldon Cheng <sheldonscyd@gmail.com>
2015-01-30 18:36:32 +0800

    Update some dest paths in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('sass', function(){
     })
   //.pipe(sass({style: 'expanded', sourcemap: false}))
   .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-  .pipe(gulp.dest('assets/css/app.css'));
+  .pipe(gulp.dest('assets/css'));
 });
 
 // Default task
@@ -30,13 +30,13 @@ gulp.task('build', function(){
   gulp.src('source/js/**')
     .pipe(gulp.dest('assets/js'));
   gulp.src('bower_components/jquery/dist/jquery.min.js')
-    .pipe(gulp.dest('assets/js/vendor/jquery.min.js'));
+    .pipe(gulp.dest('assets/js/vendor/'));
   gulp.src('bower_components/foundation/js/foundation.min.js')
-    .pipe(gulp.dest('assets/js/vendor/foundation.min.js'));
+    .pipe(gulp.dest('assets/js/vendor/'));
   gulp.src('bower_components/modernizr/modernizr.js')
-    .pipe(gulp.dest('assets/js/vendor/modernizr.js'));
+    .pipe(gulp.dest('assets/js/vendor/'));
   gulp.src('bower_components/masonry/dist/masonry.pkgd.min.js')
-    .pipe(gulp.dest('assets/js/vendor//masonry.min.js'));
+    .pipe(gulp.dest('assets/js/vendor/'));
 
   gulp.start('sass');
 });
